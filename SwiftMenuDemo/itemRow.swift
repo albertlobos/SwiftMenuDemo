8198721e9e356d2e77eaa4cc8291163ed0de47d3
Nelso Villalobos <nelso.lobos13@gmail.com>
2023-11-23 18:38:53 -0500

    Moved Images to Assets and Set Thumbnail

--- SwiftMenuDemo/itemRow.swift
+++ SwiftMenuDemo/itemRow.swift
@@ -12,8 +12,12 @@ struct itemRow: View {
     
     var body: some View {
         HStack{
-            Image(item.thumbnailImage)
-            Text(item.name)
+            Image(item.thumbnailImage);
+            
+            VStack(alignment: .leading){
+                Text(item.name);
+                Text("$\(item.price)");
+            }
         }
     }
 }

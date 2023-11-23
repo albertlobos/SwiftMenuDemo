33c758289ed1381d080809f25f3304cd50c2947a
Nelso Villalobos <nelso.lobos13@gmail.com>
2023-11-23 17:25:17 -0500

    Initial Commit

---
+++ SwiftMenuDemo/SwiftMenuDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMenuDemoApp.swift
+//  SwiftMenuDemo
+//
+//  Created by Albert Villalobos on 11/23/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMenuDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

a0e325e221388b7e7ea736837ac162806230654e
Nelso Villalobos <nelso.lobos13@gmail.com>
2023-11-23 19:06:05 -0500

    Added Restrictions and Styling

--- SwiftMenuDemo/ContentView.swift
+++ SwiftMenuDemo/ContentView.swift
@@ -26,7 +26,6 @@ struct ContentView: View {
                         }
                     }
                 }
-                
             }
             .navigationTitle("Menu")
             .listStyle(GroupedListStyle());
